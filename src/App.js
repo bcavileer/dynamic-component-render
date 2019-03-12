9b873b02de4a0c9e134fa85b0b5f916b9dedeb56
Benjamin Cavileer <bcavileer@gmail.com>
2019-03-12 14:35:54 -0400

    Demo rendering dynamic components.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+
+const Header = props => <header>{props.title}{props.children}</header>
+const Section = props => <section>{props.content}{props.children}</section>
+const Footer = props => <footer>{props.copyright}</footer>
+
+const componentManifest = {
+  'Header': Header,
+  'Section': Section,
+  'Footer': Footer,
+}
+
+const demoActivity = {
+  components: [
+    {
+      component: 'Header',
+      props: { title: 'I am a header' },
+    },
+    {
+      component: 'Section',
+      props: { content: 'I am a section' },
+      children: [
+        {
+          component: 'Section',
+          props: { content: 'I am a nested section' },
+        }
+      ],
+    },
+    {
+      component: 'Footer',
+      props: { copyright: 'HSV 2018' },
+    }
+  ]
+}
 
 class App extends Component {
-  render() {
+  constructor (props) {
+    super(props)
+    this.state = { currentActivity: demoActivity }
+    this.renderComponent = this.renderComponent.bind(this)
+  }
+
+  renderActivity () {
+    const { currentActivity } = this.state
+    const { components } = currentActivity
+    return components.map(this.renderComponent)
+  }
+
+  renderComponent (componentDesc, index) {
+    const { component, props, children } = componentDesc
+    const properties = {
+      component: componentManifest[component],
+      key: `component-${index}`,
+      children: null
+    }
+    if (children) { properties.children = children.map(this.renderComponent) }
+    return <properties.component
+      key={`component-${properties.key}`}      {...props}    >      {properties.children}    </properties.component>
+  }
+
+  render () {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.renderActivity()}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
